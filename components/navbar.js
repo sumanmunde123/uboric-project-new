f0a8382348d7900602038701f33dbd4b79632ba2
sumanmunde123 <93462136+sumanmunde123@users.noreply.github.com>
2022-01-20 23:21:32 +0530

    myproject

---
+++ components/navbar.js
@@ -0,0 +1,99 @@
+function navbar()
+
+{
+
+
+    return ` <head>
+    <div id="navbar">
+        <div id="navb">
+            <p>FOR ONLINE PAYMENT EXTRA ₹100 DISCOUNT ON ORDERS ABOVE ₹500</p>
+        </div>
+    
+       <div class="nav">
+           <input type="text" placeholder="Search here..." id="search">
+           <button id="btn"><i class="fas fa-search"></i></button>
+           <div id="webimg">
+               <img id="mainim" src="https://www.uboric.com/wp-content/uploads/2021/04/uboric.svg"/>
+           </div>
+           <div id="profilelogo">
+               <div class="logo">
+                <a href="login.html"><i class="fas fa-user-circle">
+                </i></a>
+               
+                
+               
+               </div>
+               <div class="logo">
+               <a href="cartcheck.html"> <i class="fas fa-cart-plus"></i></a>
+               </div>
+               <div id="logo2"></div>
+
+           </div>
+       </div>
+       <div class="nav">
+          <div id="sum">
+            <div class="mainnavbar" style="background-color: black; color: white;">HOME</div>
+            <div class="mainnavbar" id="hovvver">
+                <select id="cat">
+                    <option value="" >Categories</option>
+                   
+               
+            
+                </select>
+                <div id="mainhov">
+                <div id="hov">
+                <h4 class="headhover">FASHION</h4>
+                <hr>
+                <h4 class="headhover">KITCHEN</h4>
+                <hr>
+                <h4 class="headhover">ACCESSORIES</h4>
+                <hr>
+                <h4 class="headhover">HEALTH & PERSONAL CARE</h4>
+                <hr>
+                <h4 class="headhover">ELECTRONICS</h4>
+                <hr>
+                <h4 class="headhover">ELECTRICAL APPLIANCES</h4>
+                <hr>
+                <h4 class="headhover">PAINTINGS</h4>
+                <hr>
+                <h4 class="headhover">ANTIPUNCTURE LIQUID</h4>
+                
+
+
+                      </div>
+                <div id="hov2">
+                <div id="righthover">
+                <a  href="prod.html"><h3>foot wear</h3></a>
+                <a href="prod.html"><h3>mens wear</h3></a>
+                <a href="prod.html"><h3>Casual Chappal</h3></a>
+                <a href="prod.html"><h3>Casual Sandals</h3></a>
+                <a href="prod.html"><h3>Casual Party Wear</h3></a>
+                <a href="prod.html"><h3>Chappal Slliper</h3></a>
+                <a href="prod.html"><h3>Crocs</h3></a>
+                <a href="prod.html"><h3>FlipFlop</h3></a>
+                <a href="prod.html"><h3>Loafers</h3></a>
+                <a href="prod.html"><h3>Sports Sandals</h3></a>
+                </div>
+                <div id="righthover2">
+                <a href="prod.html"><h3>Product Clousers</h3></a>
+                <a href="prod.html"><h3>Lace Up </h3></a>
+                <a href="prod.html"><h3>Slip On</h3></a></div>
+                <div id="righthover3">
+                <a href="prod.html"><h3>Clothing </h3></a>
+                <a href="prod.html"><h3>Mens Clothing </h3></a>
+                <a href="prod.html"><h3>Womens Clothing</h3></a>
+                </div>
+                </div>
+                </div>
+            </div>
+            <a href="register.html"><div class="mainnavbar">Customer/Guest login</div></a>
+            <div class="mainnavbar">Merchant/Bulk Order</div>
+            <div class="mainnavbar">Become a Seller</div>
+          </div>
+       </div>
+    </div>
+
+</head>`
+}
+
+export default navbar
